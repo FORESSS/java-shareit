48a1bc39e202f0124a6af383af749c9a97241974
Сергей <master-fores@yandex.ru>
2024-07-26 09:53:14 +0300

    Добавлены DTO классы

--- src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
+++ src/main/java/ru/practicum/shareit/item/dto/ItemDto.java
@@ -1,7 +1,26 @@
 package ru.practicum.shareit.item.dto;
 
-/**
- * TODO Sprint add-controllers.
- */
+import jakarta.validation.constraints.AssertTrue;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+@Data
 public class ItemDto {
+    @NotNull
+    private long id;
+    @NotBlank
+    @Length(max = 50)
+    private String name;
+    @NotBlank
+    @Length(max = 200)
+    private String description;
+    @NotNull
+    @AssertTrue
+    private Boolean available;
+    @NotNull
+    private long owner;
+    @NotNull
+    private long request;
 }
