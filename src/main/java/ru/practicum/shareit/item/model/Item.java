cc0cd400a468be2f8a4ff0572e80114e999c10b3
Сергей <master-fores@yandex.ru>
2024-08-16 12:49:29 +0300

    Обновлены классы Item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,21 +1,25 @@
-package ru.practicum.shareit.item.dto;
+package ru.practicum.shareit.item.model;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.Length;
-import ru.practicum.shareit.booking.dto.BookingDTO;
-import ru.practicum.shareit.comment.dto.CommentDTO;
 import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.model.User;
 
-import java.util.Collection;
-
+@Entity
+@Table(name = "items")
 @Data
 @Builder
 @AllArgsConstructor
-public class ItemDTO {
+@NoArgsConstructor
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @NotBlank
     @Length(max = 50)
@@ -25,8 +29,10 @@ public class ItemDTO {
     private String description;
     @NotNull
     private Boolean available;
+    @ManyToOne
+    @JoinColumn(name = "owner_id")
+    private User owner;
+    @ManyToOne
+    @JoinColumn(name = "request_id")
     private ItemRequest request;
-    private Collection<CommentDTO> comments;
-    private BookingDTO lastBooking;
-    private BookingDTO nextBooking;
 }
--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -3,89 +3,102 @@ package ru.practicum.shareit.item.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import ru.practicum.shareit.exception.ValidationException;
-import ru.practicum.shareit.item.Item;
+import ru.practicum.shareit.comment.dto.CommentDTO;
+import ru.practicum.shareit.comment.dto.CommentMapper;
+import ru.practicum.shareit.comment.model.Comment;
+import ru.practicum.shareit.comment.repository.CommentRepository;
 import ru.practicum.shareit.item.dto.ItemDTO;
 import ru.practicum.shareit.item.dto.ItemMapper;
-import ru.practicum.shareit.item.dto.ItemUpdateDTO;
+import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.repository.ItemRepository;
+import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.util.Validator;
 
+import java.time.LocalDateTime;
 import java.util.Collection;
 import java.util.Collections;
-import java.util.stream.Collectors;
-
-import static java.util.Objects.isNull;
+import java.util.Optional;
 
 @Service
 @Slf4j
 @RequiredArgsConstructor
 public class ItemServiceImpl implements ItemService {
     private final ItemRepository itemRepository;
+    private final CommentRepository commentRepository;
     private final Validator validator;
 
     @Override
     public Collection<ItemDTO> getAllItems(long userId) {
         validator.checkUserId(userId);
-        log.info("Возврат списка всех предметов");
-        return itemRepository.getAllItems(userId).stream()
+        log.info("Получение списка всех предметов пользователя с id: {}", userId);
+        return itemRepository.findAllByOwnerId(userId).stream()
                 .map(ItemMapper::toItemDto)
                 .toList();
     }
 
     @Override
     public ItemDTO getItemById(long itemId) {
-        log.info("Возврат предмета с id: {}", itemId);
-        return itemRepository.getItemById(itemId)
-                .map(ItemMapper::toItemDto)
-                .orElseThrow(() -> new ValidationException("Предмет с id: " + itemId + " не найден"));
+        Item item = validator.validateAndGetItem(itemId);
+        ItemDTO itemDto = ItemMapper.toItemDto(item);
+        itemDto.setComments(commentRepository.findAllByItemId(itemId).stream()
+                .map(CommentMapper::toCommentDto)
+                .toList());
+        log.info("Получение предмета с id: {}", itemId);
+        return itemDto;
     }
 
     @Override
     public Collection<ItemDTO> searchItems(String text) {
-        if (isNull(text) || text.isBlank()) {
+        if (text == null || text.isBlank()) {
             return Collections.emptyList();
         }
         log.info("Поиск предметов по тексту: {}", text);
-        return itemRepository.searchItems(text).stream()
+        return itemRepository.findByNameContainingIgnoreCaseAndAvailableTrueOrDescriptionContainingIgnoreCaseAndAvailableTrue(text, text)
+                .stream()
                 .map(ItemMapper::toItemDto)
-                .collect(Collectors.toList());
+                .toList();
     }
 
     @Override
     public ItemDTO createItem(long userId, ItemDTO itemDto) {
-        validator.checkUserId(userId);
+        User user = validator.validateAndGetUser(userId);
         Item item = ItemMapper.toItem(itemDto);
-        item.setOwner(userId);
-        log.info("Создание нового предмета для пользователя с id: {}", userId);
-        return ItemMapper.toItemDto(itemRepository.createItem(item));
+        item.setOwner(user);
+        itemRepository.save(item);
+        log.info("Создание нового предмета с id: {} пользователя с id: {}", item.getId(), userId);
+        return ItemMapper.toItemDto(item);
     }
 
     @Override
-    public ItemDTO updateItem(long userId, long itemId, ItemUpdateDTO itemUpdateDTO) {
-        validator.checkUserId(userId);
-        Item item = itemRepository.getItemById(itemId).orElseThrow(() ->
-                new RuntimeException("Предмет с id: " + itemId + " не найден"));
-        validator.checkOwner(userId, item);
-        if (itemUpdateDTO.getName() != null) {
-            item.setName(itemUpdateDTO.getName());
-        }
-        if (itemUpdateDTO.getDescription() != null) {
-            item.setDescription(itemUpdateDTO.getDescription());
-        }
-        if (itemUpdateDTO.getAvailable() != null) {
-            item.setAvailable(itemUpdateDTO.getAvailable());
-        }
-        item.setOwner(userId);
-        log.info("Обновление предмета с id: {} для пользователя с id: {}", itemId, userId);
-        return ItemMapper.toItemDto(itemRepository.updateItem(itemId, item));
+    public ItemDTO updateItem(long userId, long itemId, ItemDTO newItemDto) {
+        Item item = validator.validateAndGetItem(itemId);
+        validator.checkItemOwner(userId, item);
+        Optional.ofNullable(newItemDto.getName()).ifPresent(item::setName);
+        Optional.ofNullable(newItemDto.getDescription()).ifPresent(item::setDescription);
+        Optional.ofNullable(newItemDto.getAvailable()).ifPresent(item::setAvailable);
+        itemRepository.save(item);
+        log.info("Обновление предмета с id: {} пользователя с id: {}", itemId, userId);
+        return ItemMapper.toItemDto(item);
     }
 
     @Override
     public void deleteItem(long userId, long itemId) {
         validator.checkUserId(userId);
         validator.checkItemId(itemId);
-        log.info("Удаление предмета с id: {} для пользователя с id: {}", itemId, userId);
-        itemRepository.deleteItem(itemId);
+        log.info("Удаление предмета с id: {} пользователя с id: {}", itemId, userId);
+        itemRepository.deleteById(itemId);
+    }
+
+    @Override
+    public CommentDTO addComment(long userId, long itemId, Comment comment) {
+        User author = validator.validateAndGetUser(userId);
+        Item item = validator.validateAndGetItem(itemId);
+        validator.validateUserHasPastBooking(userId);
+        comment.setCreated(LocalDateTime.now());
+        comment.setItem(item);
+        comment.setAuthor(author);
+        commentRepository.save(comment);
+        log.info("Добавлен комментарий к предмету с id: {} пользователя с id: {}", itemId, userId);
+        return CommentMapper.toCommentDto(comment);
     }
 }
