228fab73672d881d319ff5a5e769e76738005ed1
Сергей <master-fores@yandex.ru>
2024-08-16 12:47:15 +0300

    Добавлены исключения

--- src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/ErrorHandler.java
@@ -13,21 +13,42 @@ public class ErrorHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
-        log.info(ex.getMessage());
+        log.error(ex.getMessage());
         return new ErrorResponse("Ошибка валидации", ex.getMessage());
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(ValidationException.class)
     public ErrorResponse handleValidationException(ValidationException ex) {
-        log.info(ex.getMessage());
+        log.error(ex.getMessage());
         return new ErrorResponse("Ошибка валидации", ex.getMessage());
     }
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(InvalidRequestException.class)
     public ErrorResponse handleInvalidRequestException(InvalidRequestException ex) {
-        log.info(ex.getMessage());
+        log.error(ex.getMessage());
         return new ErrorResponse("Некорректный запрос", ex.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(InvalidCommentException.class)
+    public ErrorResponse handleInvalidCommentException(InvalidCommentException ex) {
+        log.error(ex.getMessage());
+        return new ErrorResponse("Ошибка добавления комментария", ex.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ExceptionHandler(InvalidBookingException.class)
+    public ErrorResponse handleInvalidBookingException(InvalidBookingException ex) {
+        log.error(ex.getMessage());
+        return new ErrorResponse("Ошибка бронирования", ex.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(PermissionDeniedException.class)
+    public ErrorResponse handlePermissionDeniedException(PermissionDeniedException ex) {
+        log.error(ex.getMessage());
+        return new ErrorResponse("Ошибка доступа", ex.getMessage());
     }
 }
