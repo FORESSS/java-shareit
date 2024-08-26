64115f4649ddc043ceac24e695b3689bb7ab67a8
Сергей <master-fores@yandex.ru>
2024-08-26 11:58:52 +0300

    test2

--- gateway/src/test/java/ru/practicum/shareit/ShareItGatewayTests.java
+++ gateway/src/test/java/ru/practicum/shareit/ShareItGatewayTests.java
@@ -1,13 +1,25 @@
 package ru.practicum.shareit;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(classes = ShareItGateway.class)
 class ShareItGatewayTests {
+    @Autowired
+    private ApplicationContext context;
 
     @Test
-    void contextLoads() {
+    void testContextLoads() {
+        assertThat(context).isNotNull();
+    }
+
+    @Test
+    void testCheckBeanLoading() {
+        assertThat(context.containsBean("shareItApp")).isTrue();
     }
 
 }
