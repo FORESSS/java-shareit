824e47ae6cc0005ba25f4b39a7e39039baba1115
Сергей <master-fores@yandex.ru>
2024-08-26 18:43:44 +0300

    test

--- src/main/java/ru/practicum/shareit/exception/PermissionDeniedException.java
+++
@@ -1,7 +0,0 @@
-package ru.practicum.shareit.exception;
-
-public class PermissionDeniedException extends RuntimeException {
-    public PermissionDeniedException(String message) {
-        super(message);
-    }
-}
