9805f1e7ddcff718ff9aa6c91f6fcb8898b4fd1e
Сергей <master-fores@yandex.ru>
2024-08-27 15:23:25 +0300

    test777999111112223

--- server/src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -9,7 +9,6 @@ import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.model.Status;
 import ru.practicum.shareit.booking.repository.BookingRepository;
 import ru.practicum.shareit.exception.InvalidCommentException;
-import ru.practicum.shareit.exception.ValidationException;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.dto.ItemWithDateDto;
