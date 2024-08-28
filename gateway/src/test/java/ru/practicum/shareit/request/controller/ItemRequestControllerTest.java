17a260aafe3757956b9407c056d111def1487f83
Сергей <master-fores@yandex.ru>
2024-08-28 17:17:34 +0300

    test54321999777888955555555

--- gateway/src/test/java/ru/practicum/shareit/request/controller/ItemRequestControllerTest.java
+++ gateway/src/test/java/ru/practicum/shareit/request/controller/ItemRequestControllerTest.java
@@ -1,46 +1,87 @@
 package ru.practicum.shareit.request.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.service.RequestClient;
 
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.practicum.shareit.util.Constants.LOCAL_DATE_TIME;
 
 @ExtendWith(MockitoExtension.class)
 class ItemRequestControllerTest {
     @InjectMocks
     private ItemRequestController itemRequestController;
-
+    @Mock
+    private RequestClient requestClient;
     private ObjectMapper mapper;
-
     private MockMvc mvc;
-
     private ItemRequestDto itemRequestDto;
 
     @BeforeEach
     void setUp() {
         mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
 
         mvc = MockMvcBuilders
                 .standaloneSetup(itemRequestController)
                 .build();
 
         itemRequestDto = ItemRequestDto.builder()
-                .description("")
-                .created(null)
+                .id(1L)
+                .description("Request")
+                .created(LOCAL_DATE_TIME)
                 .build();
     }
 
+    @Test
+    void findRequestById() throws Exception {
+        when(requestClient.findRequestById(1L))
+                .thenReturn(ResponseEntity.ok(itemRequestDto));
+
+        mvc.perform(get("/requests/{requestId}", 1L)
+                        .header("X-Sharer-User-Id", 1L))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findRequestsByUserId() throws Exception {
+        List<ItemRequestDto> requests = Collections.singletonList(itemRequestDto);
+        when(requestClient.findRequestsByUserId(1L))
+                .thenReturn(ResponseEntity.ok(requests));
+
+        mvc.perform(get("/requests")
+                        .header("X-Sharer-User-Id", 1L))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void findAllRequests() throws Exception {
+        List<ItemRequestDto> requests = Collections.singletonList(itemRequestDto);
+        when(requestClient.findAllRequests(1L))
+                .thenReturn(ResponseEntity.ok(requests));
+
+        mvc.perform(get("/requests/all")
+                        .header("X-Sharer-User-Id", 1L))
+                .andExpect(status().isOk());
+    }
     @Test
     void create() throws Exception {
         mvc.perform(post("/requests")
--- server/src/test/java/ru/practicum/shareit/ShareItTests.java
+++ server/src/test/java/ru/practicum/shareit/ShareItTests.java
@@ -4,15 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(
-		properties = {"spring.datasource.driver-class-name=org.h2.Driver",
-				"spring.datasource.url=jdbc:h2:mem:shareit",
-				"spring.datasource.username=dbuser",
-				"spring.datasource.password=12345"},
-		webEnvironment = SpringBootTest.WebEnvironment.NONE)
+        properties = {"spring.datasource.driver-class-name=org.h2.Driver",
+                "spring.datasource.url=jdbc:h2:mem:shareit",
+                "spring.datasource.username=fores",
+                "spring.datasource.password=12345"},
+        webEnvironment = SpringBootTest.WebEnvironment.NONE)
 class ShareItTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
