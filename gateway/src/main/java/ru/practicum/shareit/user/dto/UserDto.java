d05be0cc7d366000ef4a43645828ceb870eba157
Сергей <master-fores@yandex.ru>
2024-08-28 21:48:42 +0300

    Обновлены классы User

--- gateway/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
+++ gateway/src/main/java/ru/practicum/shareit/user/dto/UserDto.java
@@ -2,18 +2,14 @@ package ru.practicum.shareit.user.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 import lombok.Data;
-import org.hibernate.validator.constraints.Length;
 
 @Data
 @Builder
-public class UserDTO {
-    @NotNull
+public class UserDto {
     private long id;
     @NotBlank
-    @Length(max = 50)
     private String name;
     @NotBlank
     @Email
--- server/src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -3,8 +3,9 @@ package ru.practicum.shareit.user.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
-import ru.practicum.shareit.user.dto.UserDTO;
-import ru.practicum.shareit.user.dto.UserMapper;
+import org.springframework.transaction.annotation.Transactional;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.mapper.UserMapper;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
 import ru.practicum.shareit.util.Validator;
@@ -14,36 +15,40 @@ import java.util.Collection;
 @Service
 @Slf4j
 @RequiredArgsConstructor
+@Transactional
 public class UserServiceImpl implements UserService {
     private final UserRepository userRepository;
+    private final UserMapper userMapper;
     private final Validator validator;
 
     @Override
-    public Collection<UserDTO> getAllUsers() {
+    @Transactional(readOnly = true)
+    public Collection<UserDto> getAllUsers() {
         log.info("Получение списка всех пользователей");
         return userRepository.findAll().stream()
-                .map(UserMapper::toUserDto)
+                .map(userMapper::userToUserDto)
                 .toList();
     }
 
     @Override
-    public UserDTO getUserById(long userId) {
+    @Transactional(readOnly = true)
+    public UserDto getUserById(long userId) {
         User user = validator.validateAndGetUser(userId);
         log.info("Получение пользователя с id: {}", userId);
-        return UserMapper.toUserDto(user);
+        return userMapper.userToUserDto(user);
     }
 
     @Override
-    public UserDTO createUser(UserDTO userDto) {
+    public UserDto createUser(UserDto userDto) {
         validator.checkEmail(userDto.getEmail());
-        User user = UserMapper.toUser(userDto);
+        User user = userMapper.userDtoToUser(userDto);
         userRepository.save(user);
         log.info("Создание нового пользователя с id: {}", user.getId());
-        return UserMapper.toUserDto(user);
+        return userMapper.userToUserDto(user);
     }
 
     @Override
-    public UserDTO updateUser(long userId, UserDTO userDto) {
+    public UserDto updateUser(long userId, UserDto userDto) {
         User user = validator.validateAndGetUser(userId);
         String newEmail = userDto.getEmail();
         if (newEmail != null) {
@@ -56,11 +61,11 @@ public class UserServiceImpl implements UserService {
         }
         userRepository.save(user);
         log.info("Обновление пользователя с id: {}", userId);
-        return UserMapper.toUserDto(user);
+        return userMapper.userToUserDto(user);
     }
 
     @Override
-    public void deleteUserById(long userId) {
+    public void deleteUser(long userId) {
         validator.checkUserId(userId);
         log.info("Удаление пользователя с id: {}", userId);
         userRepository.deleteById(userId);
